d2f7ba4ce70ff0013f804fa337dcc9dcfba290e6
Julius Noack <julius.noack@netlogix.de>
2022-06-13 11:39:06 +0200

    add blacklisted controller trait to check for controllers in subscribers so we dont waste resource when not needed

---
+++ BlacklistedControllersHelper/BlacklistedControllerTrait.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Created by netlogix GmbH & Co. KG
+ *
+ * @copyright netlogix GmbH & Co. KG
+ */
+namespace nlxShopwareHelpers\BlacklistedControllersHelper;
+
+trait BlacklistedControllerTrait
+{
+    public function isBlacklistedControllerName(string $controllerName): bool
+    {
+        $blacklistedControllers = $this->getBlacklistedControllers();
+        return in_array($controllerName, $blacklistedControllers);
+    }
+
+    public function isBlacklistedController(\Enlight_Controller_Action $controller): bool
+    {
+        $blacklistedControllers = $this->getBlacklistedControllers();
+        return in_array($controller->Request()->getControllerName(), $blacklistedControllers);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getBlacklistedControllers(): array
+    {
+        if (isset($this->blacklistedControllers)) {
+            return $this->blacklistedControllers;
+        }
+
+        return [];
+    }
+}
